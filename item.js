7c5911d55327b4ef11419f301a59dc41ae321dc0
Netson <netsoncavina@gmail.com>
2021-12-28 13:43:00 -0300

    Update in items and index.html

--- item.js
+++ item.js
@@ -1,30 +1,104 @@
-class Item {
-  constructor(id, name, type, description, price) {
-    this.id = id;
-    this.name = name;
-    this.type = type;
-    this.description = description;
-    this.price = price;
-    this.image = "sprites/items/" + this.name + ".png";
+let items = [
+  {
+    id: 1,
+    name: "potion",
+    type: "healing",
+    description:
+      "A spray-type wound medicine. It restores the HP of one Pokémon by 20 points.",
+    price: 50,
+  },
+  {
+    id: 2,
+    name: "super-potion",
+    type: "healing",
+    description:
+      "A spray-type wound medicine. It restores the HP of one Pokémon by 50 points.",
+    price: 100,
+  },
+  {
+    id: 3,
+    name: "hyper-potion",
+    type: "healing",
+    description:
+      "A spray-type wound medicine. It restores the HP of one Pokémon by 200 points.",
+    price: 200,
+  },
+  {
+    id: 4,
+    name: "full-restore",
+    type: "healing",
+    description:
+      "A spray-type wound medicine. It restores the HP of one Pokémon to full.",
+    price: 500,
+  },
+  //   {
+  //     id: 5,
+  //     name: "poke-ball",
+  //     type: "pokeball",
+  //     description:
+  //       "A Poké Ball is used to catch Pokémon. It is thrown like a ball at a Pokémon to attempt to catch it.",
+  //     price: 100,
+  //   },
+];
+
+function showItems(itemList) {
+  let itemListElement = document.getElementById("item-list");
+  itemListElement.innerHTML = "";
+
+  let div = "";
+  for (let i = 0; i < itemList.length; i++) {
+    // image = `sprites/pokemon/main-sprites/${dexNumber}.png`
+    div += `<div class="form-check ">
+        <input class="form-check-input" type="radio" name="itemRadio" id="item${i}" value="${itemList[i].name}">
+        <label class="form-check-label" for="item${i}"><img src="sprites/items/${itemList[i].name}.png"/>${itemList[i].name}</label>
+      </div> `;
   }
 
-  use(pokemon) {
-    console.log("Using " + this.name + " on " + pokemon.name);
-  }
+  itemListElement.innerHTML += div;
+}
 
-  toss() {
-    console.log("Tossing " + this.name);
-  }
-
-  give(pokemon) {
-    console.log("Giving " + this.name + " to " + pokemon.name);
-  }
-
-  showItem() {
-    let item = document.getElementById(`item1`);
-    item.src = this.image;
+function findItem(item) {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].name == item) {
+      return items[i];
+    }
   }
 }
 
-// let antidote = new Item(1, "apicot-berry", "Healing", "Heals the user.", 50);
-// antidote.showItem();
+function getItemName(item) {
+  return findItem(item).name;
+}
+
+function determineItemType(item) {
+  let type = findItem(item).type;
+  return type;
+}
+
+function itemEffect(item) {
+  type = determineItemType(item);
+  itemName = getItemName(item);
+
+  if (type == "healing") {
+    if (itemName == "potion") {
+      return [20, "HP"];
+    }
+    if (itemName == "super-potion") {
+      return [50, "HP"];
+    }
+    if (itemName == "hyper-potion") {
+      return [200, "HP"];
+    }
+    if (itemName == "full-restore") {
+      return [90000, "HP"];
+    }
+  }
+
+  return false;
+}
+
+function useItem(pokemon) {
+  let item = document.querySelector("input[name=itemRadio]:checked").value;
+  return itemEffect(item);
+}
+
+showItems(items);
--- pokemon.js
+++ pokemon.js
@@ -1,45 +1,186 @@
 class Pokemon {
-  constructor(name, dexNumber, type, level, hp, attackList) {
+  constructor(name, dexNumber, type, hp, level) {
     this.name = name;
     this.dexNumber = dexNumber;
     this.type = type;
-    this.level = level;
     this.hp = hp;
-    this.currentHp = hp;
-    this.attackList = attackList;
-    this.image = `sprites/pokemon/main-sprites/firered-leafgreen/${this.dexNumber}.png`;
-  }
+    this.level = level;
 
-  getType() {
-    return this.type;
-  }
+    // this.currentHP = hp;
 
-  getHP() {
-    return this.hp;
-  }
+    // this.image = `sprites/pokemon/main-sprites/firered-leafgreen/${dexNumber}.png`;
+    this.image = `sprites/pokemon/main-sprites/${dexNumber}.png`;
 
-  updateHp(damage) {
-    this.currentHp -= damage;
-    return this.currentHp;
-  }
+    this.updateHealthbar = function (spriteLocation, currentHP) {
+      let healthbar = document.getElementById(`hp${spriteLocation}`);
+      let currentPercentage = (currentHP * 100) / this.hp;
+      console.log(currentPercentage);
+      healthbar.innerHTML = `${currentHP}/${this.hp}`;
 
-  getAttackList() {
-    return this.attackList;
-  }
+      if (currentPercentage >= 100) {
+        healthbar.style.width = "100%";
+        healthbar.className = "progress-bar";
+      }
+      if (currentHP <= 0) {
+        currentPercentage = 0;
+        healthbar.style = `width: 0%`;
+        healthbar.innerHTML = 0;
+      } else {
+        if (currentPercentage >= 50) {
+          healthbar.className = "progress-bar bg-success";
+        }
+        if (currentPercentage < 50 && currentPercentage >= 25) {
+          healthbar.className =
+            "progress-bar progress-bar-striped progress-bar-animated bg-warning";
+        }
+        if (currentPercentage < 25) {
+          healthbar.className =
+            "progress-bar progress-bar-striped progress-bar-animated bg-danger";
+        }
+        if (this.hp >= hp) {
+          this.hp = hp;
+          // currentHP = hp;
+        }
+        healthbar.style = `width: ${currentPercentage}%`;
 
-  showPokemon(spriteLocation) {
-    let sprite = document.getElementById(`sprite${spriteLocation}`);
-    sprite.src = this.image;
+        healthbar.innerHTML = `${currentHP}/${this.hp}`;
+      }
+    };
+
+    this.showPokemon = function (spriteLocation) {
+      let sprite = document.getElementById(`sprite${spriteLocation}`);
+      sprite.src = this.image;
+    };
+    this.getType = function () {
+      return this.type;
+    };
+
+    this.info = function () {
+      return (
+        "The " +
+        this.name +
+        " is a " +
+        this.type +
+        " type pokemon that is level " +
+        this.level +
+        "."
+      );
+    };
+
+    this.isAlive = function () {
+      if (this.hp <= 0) {
+        console.log("The " + this.name + " is fainted.");
+      }
+    };
+
+    this.attack_effectiveness = function (type1, type2) {
+      if (type1 == type2) {
+        return 1;
+      }
+      if (type1 == "fire" && type2 == "grass") {
+        return 2;
+      }
+
+      if (type1 == "fire" && type2 == "water") {
+        return 0.5;
+      }
+      if (type1 == "water" && type2 == "fire") {
+        return 2;
+      }
+      if (type1 == "water" && type2 == "grass") {
+        return 0.5;
+      }
+      if (type1 == "grass" && type2 == "water") {
+        return 2;
+      }
+      if (type1 == "grass" && type2 == "fire") {
+        return 0.5;
+      }
+    };
+
+    this.attackList = [];
+
+    this.showAttackList = function (pokemonLocation) {
+      let attackList = document.getElementById(`attackList${pokemonLocation}`);
+      attackList.innerHTML = "";
+
+      let div = "";
+      for (let i = 0; i < this.attackList.length; i++) {
+        div += `<div class="form-check ">
+          <input class="form-check-input" type="radio" name="attackRadio${pokemonLocation}" id="attack${i}" value="${this.attackList[i]}">
+          <label class="form-check-label" for="attack${i}">${this.attackList[i]}</label>
+        </div> `;
+      }
+
+      attackList.innerHTML += div;
+    };
+
+    this.attack = function (target_pokemon) {
+      this.multiplier = this.attack_effectiveness(
+        this.type,
+        target_pokemon.type
+      );
+      let message = "";
+      this.damage = this.level * 2 * 2 * this.multiplier;
+      //   console.log(target_pokemon.hp);
+
+      console.log(`${this.name} attacked ${target_pokemon.name}`);
+      if (this.multiplier == 0.5) {
+        message += `Not very effective `;
+      } else if (this.multiplier == 1) {
+        message += `Effective `;
+      } else if (this.multiplier == 2) {
+        message += `Super effective! `;
+      }
+
+      target_pokemon.hp -= this.damage;
+      this.updateHealthbar(2, target_pokemon.hp);
+      // console.log(`${target_pokemon.name}'s hp is now ${target_pokemon.hp}`);
+      target_pokemon.isAlive();
+      let array = [this.damage, message];
+      return array;
+    };
+
+    this.healPokemon = function (healValue) {
+      if (this.hp <= hp && this.hp + healValue <= hp) {
+        this.hp += healValue;
+        this.updateHealthbar(2, hp);
+      } else if (this.hp + healValue > hp) {
+        this.hp = hp;
+        this.updateHealthbar(2, hp);
+      } else {
+        this.hp = hp;
+        this.updateHealthbar(2, hp);
+      }
+    };
   }
 }
 
-let charmander = new Pokemon("Charmander", 4, "fire", 5, 39, [
-  "Scratch",
-  "Growl",
-  "Ember",
-  "Smokescreen",
-]);
-charmander.showPokemon(1);
+function showOutPut(attacker_pokemon, attack, damage, target_pokemon, message) {
+  let output = document.getElementById("output");
+  output.innerHTML = "";
+  // output.innerHTML += `<p>${attacker_pokemon.name}'s hp is now ${attacker_pokemon.hp}</p>`;
+  output.innerHTML += `<p>${attacker_pokemon.name} attacked ${target_pokemon.name} with ${attack}</p>`;
+  output.innerHTML += `<p>${message}</p>`;
+  // output.innerHTML += `<p>${message}</p>`;
+  output.innerHTML += `<p>${target_pokemon.name}'s lost ${damage} hp</p>`;
+  output.innerHTML += `<p>${target_pokemon.name}'s hp is now ${target_pokemon.hp}</p>`;
+}
 
-let bulbasaur = new Pokemon("Bulbasaur", 1, "grass", 5, 39, []);
-bulbasaur.showPokemon(2);
+function attack(attacker_pokemon, attack, target_pokemon) {
+  let hit = document.getElementById(`attack${attack}`);
+  if (hit.checked) {
+    let info = attacker_pokemon.attack(target_pokemon);
+    damage = info[0];
+    showOutPut(attacker_pokemon, hit.value, damage, target_pokemon, info[1]);
+  }
+}
+
+let charmander = new Pokemon("Charmander", 4, "fire", 100, 5);
+charmander.attackList = ["Scratch", "Growl", "Ember", "Smokescreen"];
+charmander.showPokemon(1);
+charmander.showAttackList(1);
+let squirtle = new Pokemon("Squirtle", 7, "water", 100, 5);
+squirtle.attackList = ["Tackle", "Tail Whip", "Water Gun", "Withdraw"];
+squirtle.showPokemon(2);
+squirtle.showAttackList(2);
