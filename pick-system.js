a85a73691c0b38a4136450a4d0a2f388ff6487ef
Netson <netsoncavina@gmail.com>
2021-12-29 11:36:28 -0300

    Added pick system for player pokemon and computer pokemon

--- pick-system.js
+++ pick-system.js
@@ -1,15 +1,39 @@
 import { pokemons } from "./pokemon_data.js";
 
-export function pickPokemon() {
+export function pickPokemon(position) {
   let pick = document.querySelector("input[name=pokemonRadio]:checked").value;
-  showPokemon(pick);
+
+  showPokemon(pick, position);
+  showMoves(pick, position);
+  //   showBattleTitle();
   return pick;
 }
 
-function showPokemon(pokemon) {
-  let sprite = document.getElementById(`sprite1`);
+function showBattleTitle() {
+  let title = document.getElementById("battleTitle");
+  title.innerHTML = "Time for battle!";
+}
+
+function showPokemon(pokemon, position) {
+  let sprite = document.getElementById(`sprite${position}`);
   let pokemonImage = getPokemonImage(pokemon);
   sprite.src = pokemonImage;
+}
+
+function showMoves(pokemon, position) {
+  let moveList = getPokemonMoves(pokemon);
+  let attackList = document.getElementById(`attackList${position}`);
+  attackList.innerHTML = "";
+
+  let div = "";
+  for (let i = 0; i < moveList.length; i++) {
+    div += `<div class="form-check ">
+          <input class="form-check-input" type="radio" name="attackRadio1" id="attack${i}" value="${moveList[i]}">
+          <label class="form-check-label" for="attack${i}">${moveList[i]}</label>
+        </div> `;
+  }
+
+  attackList.innerHTML += div;
 }
 
 function getPokemonImage(pokemon) {
@@ -20,7 +44,20 @@ function getPokemonImage(pokemon) {
   }
 }
 
-let button = document.getElementById("pickButton");
-button.addEventListener("click", function () {
-  pickPokemon();
+function getPokemonMoves(pokemon) {
+  for (let i = 0; i < pokemons.length; i++) {
+    if (pokemons[i].name == pokemon) {
+      return pokemons[i].moves;
+    }
+  }
+}
+
+let playerButton = document.getElementById("playerButton");
+playerButton.addEventListener("click", function () {
+  pickPokemon(1);
+});
+
+let computerButton = document.getElementById("computerButton");
+computerButton.addEventListener("click", function () {
+  pickPokemon(2);
 });
